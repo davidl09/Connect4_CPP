1fc1b235aef20c669446425de8ed72af5942947e
davidl09 <114118694+davidl09@users.noreply.github.com>
2023-06-27 13:58:38 -0400

    Update minimaxbit.hpp

--- include/minimaxbit.hpp
+++ include/minimaxbit.hpp
@@ -3,62 +3,64 @@
 
 #include <memory>
 #include <algorithm>
-#include <thread>
-#include <atomic>
+#include <stdexcept>
 
 #include "bitboard.hpp"
 #include "ltable.hpp"
+
+#define ai yellow
+#define human red
 
 struct minmax_ret{
     int column;
     int score;
 };
 
-#define ai yellow
-#define human red
-
+struct MiniMaxNode{
+    BitBoard board;
+    std::vector<std::unique_ptr<MiniMaxNode>> child_nodes;
+    
+    MiniMaxNode(BitBoard& newboard) : board(newboard){};
+    
+    void add_layers(int layers, colour playing){
+        assert(child_nodes.size() == 0);
+        
+        for(int i = 0; i < 7; i++){
+            if(board.is_legal_move(search_order[i])){
+                child_nodes.push_back(std::make_unique<MiniMaxNode>(board.place_token()));
+            }
+        }
+        //recursively populate the search tree
+        for(auto& node : child_nodes){
+            if(node){
+                node.add_layers(layers - 1, 1 - playing);
+            }
+        }
+        return;
+    }
+};
 
 class MiniMaxBit{
     private:
-    const int search_order[7] = {3,2,4,1,5,0,6};
+    
     const int _depth;
+    std::unique_ptr<MiniMaxNode> head;
 
     public:
-    MiniMaxBit(int depth_t) : _depth(depth_t){};
-
+    MiniMaxBit(int depth_t) : _depth(depth_t){
+        
+    };
+    
+    static const int search_order[7] = {3,2,4,1,5,0,6};
     constexpr int getdepth(){
         return _depth;
     }
-/*
-    int best_move(const std::unique_ptr<BitBoard>& board){
-        std::vector<minmax_ret> moves;
-        moves.reserve(7);
-
-        std::vector<std::thread> move_threads;
-        move_threads.reserve(7);
-
-        for(int i = 0; i < 7; i++){
-            moves.push_back((minmax_ret){search_order[i], INT32_MIN});
-
-            move_threads.push_back(std::thread(threaded_minimax, board->self(), &(moves[i]), this->_depth));
-        }
-
-        for(int i = 0; i < 7; i++){
-
-        }
-
-    }
-
-    static void threaded_minimax(uint64_t board[2], minmax_ret* retval, const int depth){
-        auto ptr = std::make_unique<BitBoard>(board);
-        *retval = MiniMaxBit(depth).minimax(ptr, nullptr, false, depth - 1, INT32_MIN, INT32_MAX);
-    }
-*/
+    /*
     minmax_ret minimax(const std::unique_ptr<BitBoard>& board, LookupTable* ltable, bool maximizing, int depth, int alpha, int beta){
 
         //leaf node conditions
         if(depth == 0)
-            return ((minmax_ret){.column =  -1, .score = board->score(ai)/*ltable->consult(board->self())*/});
+            return ((minmax_ret){.column =  -1, .score = board->score(ai)/*ltable->consult(board->self())/});
         if(board->isdraw())
             return(((minmax_ret){.column = -1, .score = 0}));
         if(board->iswin(ai))
@@ -89,6 +91,7 @@ class MiniMaxBit{
         }
         return best_move;
     }
+    */
     
 };
 
