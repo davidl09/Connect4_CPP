d82830d38d24badccdafe0fd09b0f236bfc13488
David <dalaeer@gmail.com>
2023-07-12 14:49:18 -0400

    cleanup

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 #include "ltable.hpp"
 #include "minimaxbit.hpp"
 
-extern "C"
-{
-    
-}
 
 int main(int argc, char* argv[]){
     std::unique_ptr<BitBoard> b(std::make_unique<BitBoard>());
@@ -15,18 +11,18 @@ int main(int argc, char* argv[]){
     LookupTable table;
     int minimaxdepth;
     try{
-        minimaxdepth = (argc > 1 ? std::stoi(argv[1]) : 11);
-    }catch(...){std::cout << ("Invalid depth option, using default=11\n");}
+        minimaxdepth = (argc > 1 ? std::stoi(argv[1]) : 10);
+    }catch(...){std::cout << ("Invalid depth option, using default=10\n");}
 
     MiniMaxBit algo = MiniMaxBit(minimaxdepth);
-    minmax_ret score_col;
+    int score_col;
 
     b->print_board();
 
 
     int move;
     while(true){
-        
+
         std::cout << "Enter your move\n";
         std::cin >> move;
 
@@ -39,10 +35,10 @@ int main(int argc, char* argv[]){
         b->print_board();
 
         std::cout << "AI plays...\n";
-        score_col = algo.minimax(b, &table, true, algo.getdepth(), INT32_MIN, INT32_MAX);
-        std::cout << "AI played in column: " << score_col.column << "\n";
+        score_col = algo.best_move(b);
+        std::cout << "AI played in column: " << score_col << "\n";
 
-        b->place_token(score_col.column, ai);
+        b->place_token(score_col, ai);
         b->print_board();
 
         if(b->iswin(ai)){
